ab6a626eb3913230ea96c5a2656dcfbfb65d37a7
Oscar Saxlund <oscar.saxlund@skola.sala.se>
2025-11-19 14:53:39 +0100

    yes

--- Slå_Flugan/Form1.Designer.cs
+++ Slå_Flugan/Form1.Designer.cs
@@ -32,7 +32,21 @@
             geting = new PictureBox();
             timer1 = new System.Windows.Forms.Timer(components);
             button1 = new Button();
+            button2 = new Button();
+            geting1 = new PictureBox();
+            geting2 = new PictureBox();
+            geting3 = new PictureBox();
+            geting4 = new PictureBox();
+            geting5 = new PictureBox();
+            geting6 = new PictureBox();
+            timer2 = new System.Windows.Forms.Timer(components);
             ((System.ComponentModel.ISupportInitialize)geting).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)geting1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)geting2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)geting3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)geting4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)geting5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)geting6).BeginInit();
             SuspendLayout();
             // 
             // geting
@@ -45,25 +59,118 @@
             geting.SizeMode = PictureBoxSizeMode.StretchImage;
             geting.TabIndex = 0;
             geting.TabStop = false;
+            geting.Visible = false;
             geting.Click += pictureBox1_Click;
             // 
             // timer1
             // 
-            timer1.Enabled = true;
             timer1.Interval = 500;
             timer1.Tick += timer1_Tick;
             // 
             // button1
             // 
-            button1.BackColor = Color.Red;
-            button1.Location = new Point(334, 22);
+            button1.BackColor = Color.Lime;
+            button1.Location = new Point(276, 2);
             button1.Name = "button1";
-            button1.Size = new Size(135, 49);
+            button1.Size = new Size(117, 63);
             button1.TabIndex = 1;
-            button1.Text = "PLAY";
+            button1.Text = "Easy mode";
             button1.UseVisualStyleBackColor = false;
-            button1.Visible = false;
             button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Red;
+            button2.Location = new Point(416, 2);
+            button2.Name = "button2";
+            button2.Size = new Size(117, 63);
+            button2.TabIndex = 2;
+            button2.Text = "Hard Mode";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // geting1
+            // 
+            geting1.BackColor = Color.Transparent;
+            geting1.Image = Properties.Resources.geting2;
+            geting1.Location = new Point(366, 141);
+            geting1.Name = "geting1";
+            geting1.Size = new Size(90, 91);
+            geting1.SizeMode = PictureBoxSizeMode.StretchImage;
+            geting1.TabIndex = 3;
+            geting1.TabStop = false;
+            geting1.Visible = false;
+            geting1.Click += pictureBox1_Click_1;
+            // 
+            // geting2
+            // 
+            geting2.BackColor = Color.Transparent;
+            geting2.Image = Properties.Resources.geting2;
+            geting2.Location = new Point(128, 286);
+            geting2.Name = "geting2";
+            geting2.Size = new Size(90, 91);
+            geting2.SizeMode = PictureBoxSizeMode.StretchImage;
+            geting2.TabIndex = 4;
+            geting2.TabStop = false;
+            geting2.Visible = false;
+            geting2.Click += pictureBox2_Click;
+            // 
+            // geting3
+            // 
+            geting3.BackColor = Color.Transparent;
+            geting3.Image = Properties.Resources.geting2;
+            geting3.Location = new Point(575, 252);
+            geting3.Name = "geting3";
+            geting3.Size = new Size(90, 91);
+            geting3.SizeMode = PictureBoxSizeMode.StretchImage;
+            geting3.TabIndex = 5;
+            geting3.TabStop = false;
+            geting3.Visible = false;
+            geting3.Click += pictureBox3_Click;
+            // 
+            // geting4
+            // 
+            geting4.BackColor = Color.Transparent;
+            geting4.Image = Properties.Resources.geting2;
+            geting4.Location = new Point(211, 83);
+            geting4.Name = "geting4";
+            geting4.Size = new Size(90, 91);
+            geting4.SizeMode = PictureBoxSizeMode.StretchImage;
+            geting4.TabIndex = 6;
+            geting4.TabStop = false;
+            geting4.Visible = false;
+            geting4.Click += pictureBox4_Click;
+            // 
+            // geting5
+            // 
+            geting5.BackColor = Color.Transparent;
+            geting5.Image = Properties.Resources.geting2;
+            geting5.Location = new Point(430, 304);
+            geting5.Name = "geting5";
+            geting5.Size = new Size(90, 91);
+            geting5.SizeMode = PictureBoxSizeMode.StretchImage;
+            geting5.TabIndex = 7;
+            geting5.TabStop = false;
+            geting5.Visible = false;
+            geting5.Click += pictureBox5_Click;
+            // 
+            // geting6
+            // 
+            geting6.BackColor = Color.Transparent;
+            geting6.Image = Properties.Resources.geting2;
+            geting6.Location = new Point(613, 71);
+            geting6.Name = "geting6";
+            geting6.Size = new Size(90, 91);
+            geting6.SizeMode = PictureBoxSizeMode.StretchImage;
+            geting6.TabIndex = 8;
+            geting6.TabStop = false;
+            geting6.Visible = false;
+            geting6.Click += pictureBox6_Click;
+            // 
+            // timer2
+            // 
+            timer2.Interval = 500;
+            timer2.Tick += timer2_Tick;
             // 
             // Form1
             // 
@@ -72,12 +179,25 @@
             BackgroundImage = Properties.Resources.Skärmbild_2025_11_10_152530;
             BackgroundImageLayout = ImageLayout.Stretch;
             ClientSize = new Size(800, 450);
+            Controls.Add(geting6);
+            Controls.Add(geting5);
+            Controls.Add(geting4);
+            Controls.Add(geting3);
+            Controls.Add(geting2);
+            Controls.Add(geting1);
+            Controls.Add(button2);
             Controls.Add(button1);
             Controls.Add(geting);
             DoubleBuffered = true;
             Name = "Form1";
             Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)geting).EndInit();
+            ((System.ComponentModel.ISupportInitialize)geting1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)geting2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)geting3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)geting4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)geting5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)geting6).EndInit();
             ResumeLayout(false);
         }
 
@@ -86,5 +206,13 @@
         private PictureBox geting;
         private System.Windows.Forms.Timer timer1;
         private Button button1;
+        private Button button2;
+        private PictureBox geting1;
+        private PictureBox geting2;
+        private PictureBox geting3;
+        private PictureBox geting4;
+        private PictureBox geting5;
+        private PictureBox geting6;
+        private System.Windows.Forms.Timer timer2;
     }
 }
--- Slå_Flugan/Form1.cs
+++ Slå_Flugan/Form1.cs
@@ -9,9 +9,12 @@ namespace Slå_Flugan
 
         private void pictureBox1_Click(object sender, EventArgs e)
         {
+            geting.Visible = false;
             timer1.Stop();
             MessageBox.Show("Grattis! du fick getingen!");
             button1.Visible = true;
+            button2.Visible = true;
+
         }
 
         private void timer1_Tick(object sender, EventArgs e)
@@ -19,12 +22,106 @@ namespace Slå_Flugan
             geting.Top = Random.Shared.Next(100, 300);
             geting.Left = Random.Shared.Next(0, 600);
             geting.BringToFront();
+
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            geting.Visible = true;
             timer1.Start();
             button1.Visible = false;
+            button2.Visible = false;
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            geting1.Visible = true;
+            geting2.Visible = true;
+            geting3.Visible = true;
+            geting4.Visible = true;
+            geting5.Visible = true;
+            geting6.Visible = true;
+            timer2.Start();
+            button1.Visible = false;
+            button2.Visible = false;
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            geting1.Top = Random.Shared.Next(100, 300);
+            geting1.Left = Random.Shared.Next(0, 600);
+            geting1.BringToFront();
+            
+            geting2.Top = Random.Shared.Next(100, 300);
+            geting2.Left = Random.Shared.Next(0, 600);
+            geting2.BringToFront();
+            
+            geting3.Top = Random.Shared.Next(100, 300);
+            geting3.Left = Random.Shared.Next(0, 600);
+            geting3.BringToFront();
+
+            geting4.Top = Random.Shared.Next(100, 300);
+            geting4.Left = Random.Shared.Next(0, 600);
+            geting4.BringToFront();
+
+            geting5.Top = Random.Shared.Next(100, 300);
+            geting5.Left = Random.Shared.Next(0, 600);
+            geting5.BringToFront();
+
+            geting6.Top = Random.Shared.Next(100, 300);
+            geting6.Left = Random.Shared.Next(0, 600);
+            geting6.BringToFront();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            geting2.Visible = false;
+            check();
+
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            geting4.Visible = false;
+            check();
+        }
+
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+            geting1.Visible = false;
+            check();
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            geting5.Visible = false;
+            check();
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            geting3.Visible = false;
+            check();
+        }
+
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+            geting6.Visible = false;
+            check();
+        }
+        private void check()
+        {
+            if (geting1.Visible == false && geting2.Visible == false && geting3.Visible == false && geting4.Visible == false && geting5.Visible == false && geting5.Visible == false)
+            {
+                timer2.Stop();
+                MessageBox.Show("Grattis! du fick getingen!");
+                button1.Visible = true;
+                button2.Visible = true;
+            }
+                
+        }
+
+        
+        
     }
 }
